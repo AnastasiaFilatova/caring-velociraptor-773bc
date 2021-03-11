62953440b4d7fe5c76891825fe976d266c8febb3
Stackbit Code Editor <support@stackbit.com>
2021-03-11 14:56:55 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -10,7 +10,7 @@ export default class Action extends React.Component {
             <a href={withPrefix(_.get(action, 'url', null))}
               {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
               {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
-              className={classNames({'btn': _.get(action, 'style', null) !== 'link', 'btn--secondary': _.get(action, 'style', null) === 'secondary'})}>{_.get(action, 'label', null)}</Link>
+              className={classNames({'btn': _.get(action, 'style', null) !== 'link', 'btn--secondary': _.get(action, 'style', null) === 'secondary'})}>{_.get(action, 'label', null)}</a>
         );
     }
 }
