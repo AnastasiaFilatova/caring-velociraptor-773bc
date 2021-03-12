7de6f4d0674c74691c8efd25f7b39a0f54c326a5
Stackbit Code Editor <support@stackbit.com>
2021-03-12 10:17:32 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import {withPrefix, attribute} from '../utils';
 import '../sass/main.scss';
 import Header from './Header';
 import Footer from './Footer';
-import {GridSection} from "../components/"
+import {GridSection} from "../components/Gr"
 
 export default class Body extends React.Component {
     render() {
