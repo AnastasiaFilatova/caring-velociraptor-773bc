bc2882c02900c93649038225f27b15a6e5281f44
Stackbit Code Editor <support@stackbit.com>
2021-03-12 10:26:32 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -51,7 +51,6 @@ export default class Body extends React.Component {
                     <div className="container container--lg">
                 
                 <div className="section container">
-                  {_.get(feature, 'image', null) && (
                   <div className={classNames('cell', 'section__media', {'section__media--right': _.get(feature, 'image_position', null) === 'right'})}>
                     <img src={withPrefix(_.get(feature, 'image', null))} alt={_.get(feature, 'image_alt', null)} />
                   </div>
