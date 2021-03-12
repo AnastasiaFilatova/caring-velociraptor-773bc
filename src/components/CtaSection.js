3ac38678156628fa423f0dcfe07cf26546cc5abb
Stackbit Code Editor <support@stackbit.com>
2021-03-12 10:58:28 +0000

    CtaSection.js: changed

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -25,7 +25,7 @@ export default class CtaSection extends React.Component {
                     <img src="/images/AWS-Developer-Associate.png" alt="AWS-Developer-Associate certificate" />
                     <img src="/images/AWS-SysOpsAdmin-Associate.png" alt="AWS-SysOpsAdmin-Associate certificate" />
                     <img src="/images/AWS-security-Specialty" alt="AWS-security-Specialty certificate" />
-                    <img src="/images/atlas_copco.png" alt="atlas copco" />
+                    <img src="/images/Terraform-Assiciate-badge.png" alt="atlas copco" />
                     <img src="/images/atlas_copco.png" alt="atlas copco" />
                     <img src="/images/atlas_copco.png" alt="atlas copco" />
                     {_.get(section, 'actions', null) && (
