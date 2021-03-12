8c4ab39069bb1f6a2ed9fd65f4d9a9e52b06d7ae
Stackbit Code Editor <support@stackbit.com>
2021-03-12 10:28:06 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -58,7 +58,7 @@ export default class Body extends React.Component {
                   <div className="section__body cell">
                       <h3 className="section__title"><div id="">Title</div></h3>
                      
-                      <h2 className="section__title">{_.get(feature, 'title', null)}</h2>
+                      <h2 className="section__title">Title</h2>
                    
                     {_.get(feature, 'content', null) && (
                     <div className="section__copy">
