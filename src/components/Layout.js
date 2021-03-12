5450170f3cbcddacc434f6e44ed5a6ec787d84d7
Stackbit Code Editor <support@stackbit.com>
2021-03-12 10:19:05 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,6 @@ export default class Body extends React.Component {
                   <Header {...this.props} />
                   <main id="content" className="site-content">
                     {this.props.children}
-                    <GridSection {...this.props} />
                   </main>
                   <Footer {...this.props} />
                 </div>
