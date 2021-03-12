3ff5839baf3554cc3f4e890e01098f6859d7c93e
Stackbit Code Editor <support@stackbit.com>
2021-03-12 10:07:16 +0000

    page.js: changed

--- src/templates/page.js
+++ src/templates/page.js
@@ -4,7 +4,6 @@ import {graphql} from 'gatsby';
 
 import {Layout} from '../components/index';
 import {htmlToReact} from '../utils';
-import {markdownify} from '../utils';
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
