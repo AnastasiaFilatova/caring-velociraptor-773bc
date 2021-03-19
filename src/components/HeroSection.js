a96029cbcb75dca821dd370f2cb5f8be1231006b
Stackbit Code Editor <projects@stackbit.com>
2021-03-19 12:50:33 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -35,7 +35,7 @@ export default class HeroSection extends React.Component {
                     </div>
                     )}
                     <div class="separator"></div>
-                    <a href="https://fivexl.io"><img src="/images/FXLBADGE-768x887.png" alt="fivexl icon" className=""/></a>
+                    <a href="https://fivexl.io"><img src="/images/FXLBADGE-768x887.png" alt="fivexl icon" className="fivexl"/></a>
                   </div>
                 </div>
               </div>
