a94cfe105f1f7d9d371d9e6b463206df153ca3d9
Stackbit Code Editor <projects@stackbit.com>
2021-03-21 21:25:17 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -12,7 +12,7 @@ export default class FeaturesSection extends React.Component {
               <div className="container container--lg">
                   <div className="flex flex--middle flex-center flex-col-2">
                       <div className="cell section__media">
-                          <img src="/images/about_bg.jpg" alt="about" />
+                          <img src="/images/about-section-bg.jpg" alt="about" />
                       </div>
                        <div className="section__body cell">
                            <h3 className="section__title">About Andrey</h3>
