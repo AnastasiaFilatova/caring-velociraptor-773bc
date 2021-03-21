fab86fc6935bfc9915e43b3e621d93a03e3cbc5b
Stackbit Code Editor <projects@stackbit.com>
2021-03-21 21:31:21 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -25,7 +25,8 @@ export default class FeaturesSection extends React.Component {
                               <p>Co-Founder at FivexL. Cloud Engineering Specialists</p>
                               <p>Co-host at DevSecOps Talks Podcast</p>
                               <p>Meetups organizer (Cloud Native Las Palmas de Gran Canaria, HashiCorp User Group Las Palmas de Gran Canaria).
-</p>
+                                  
+                              </p>
                         </div>
                     </div>
                 </div>
