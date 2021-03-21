2641cde30dec52595d6b394396668c9661ffc854
Stackbit Code Editor <projects@stackbit.com>
2021-03-21 21:39:23 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -53,7 +53,7 @@ export default class FeaturesSection extends React.Component {
                               <img src="/images/AWS-Developer-Associate.png" alt="AWS-Developer-Associate certificate" />
                               <img src="/images/AWS-SysOpAdmin-Associate.png" alt="AWS-SysOpAdmin-Associate certificate" /><br />
                               <img src="/images/AWS-Security-Specialty.png" alt="AWS-Security-Specialty certificate" />
-                                <img src="/images/Terraform-Associate-Badge.png" alt="Terraform-Associate-Badge png" />                  
+                              <img src="/images/Terraform-Associate-Badge.png" alt="Terraform-Associate-Badge png" />                  
                           </div>
                       </div>
                       
