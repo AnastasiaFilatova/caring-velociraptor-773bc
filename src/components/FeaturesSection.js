5fcb3631ce865c854b631e3e9ca33654dd5b1545
Stackbit Code Editor <projects@stackbit.com>
2021-03-21 21:07:24 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -17,7 +17,7 @@ export default class FeaturesSection extends React.Component {
                        <div className="section__body cell">
                            <h3 className="section__title">About Andrey</h3>
                            <div className="section__copy">
-                      <p>hello from Andrey</p>
+                            <p>hello from Andrey</p>
                     </div>
                        </div>
                       </div>
