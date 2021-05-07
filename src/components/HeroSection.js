07e997489c31c7862afcb08bbf775f646f5f850b
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 12:32:11 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -37,7 +37,9 @@ export default class HeroSection extends React.Component {
             <section class="section hero bg-image bg-dark section--padding">
             <div class="bg-image__image"></div>
             <div class="container container--lg">
-            <div class="flex flex--middle flex--center flex--col-2 align-center"><div class="cell section__body"><h1 class="section__title">ADREY DEVYATKIN</h1><div class="section__copy"><p>CI/CD. DevSecOps. Cloud infrastructure.</p></div></div></div></div></section>
+            <div class="flex flex--middle flex--center flex--col-2 align-center">
+            <div class="cell section__body">
+            <h1 class="section__title">ADREY DEVYATKIN</h1><div class="section__copy"><p>CI/CD. DevSecOps. Cloud infrastructure.</p></div></div></div></div></section>
 
         );
     }
